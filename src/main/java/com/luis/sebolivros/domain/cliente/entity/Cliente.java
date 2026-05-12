8b39e22380025da581fec438b0b1649be4b9df07
LuisGustav18 <luis0gustavo43@gmail.com>
2026-05-12 14:50:16 -0300

    Resolvendo erro de listagem infinita

--- src/main/java/com/luis/sebolivros/domain/cliente/entity/Cliente.java
+++ src/main/java/com/luis/sebolivros/domain/cliente/entity/Cliente.java
@@ -1,5 +1,6 @@
 package com.luis.sebolivros.domain.cliente.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.luis.sebolivros.domain.carrinho.entity.Carrinho;
 import com.luis.sebolivros.domain.cliente.dto.ClienteDTO;
 import com.luis.sebolivros.domain.common.enums.Perfil;
@@ -17,6 +18,7 @@ public class Cliente extends Usuario {
 
     // cascade -> Permite adicionar um carrinho sem id do cliente ainda salvo
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    @JsonIgnore
     private List<Carrinho> carrinhos = new ArrayList<>();
 
     public Cliente(){
