dba35286a9a4a92ca4bdd14c4857d45b6a829f1d
LuisGustav18 <luis0gustavo43@gmail.com>
2026-05-14 15:14:22 -0300

    Alterando nome de caminho de ItemCarrinhResource

--- src/main/java/com/luis/sebolivros/domain/itemCarrinho/resource/ItemCarrinhoResource.java
+++ src/main/java/com/luis/sebolivros/domain/itemCarrinho/resource/ItemCarrinhoResource.java
@@ -14,7 +14,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "ItensCarrinho")
+@RequestMapping(value = "itensCarrinho")
 public class ItemCarrinhoResource {
 
     @Autowired
